1a943fc6c0669bfb406812792aaec141b3027583
Ronin619 <eytam619@gmail.com>
2025-12-24 18:22:49 -0500

    implement landing page

---
+++ app/addContact/pages.js
@@ -0,0 +1,11 @@
+"use client";
+import Link from "next/link";
+
+export default function AddContact() {
+  return (
+    <div>
+      <h2>Let&apos;s add a contact!!!!</h2>
+      <Link href="/">Back</Link>
+    </div>
+  )
+}
